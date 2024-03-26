7156337d5d028cb4784cd865afc0077ef271e4f0
Luhuanz <Asizh207829897@163.com>
2024-03-26 08:51:14 +0800

    message: 增加注释

---
+++ cuda/2_cuda_mul/matrix_mul_test.cu
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<cuda.h>
+#include"cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "math.h"
+#include "time.h"
+#include <iostream>
+#include <fstream>
+#include<iomanip>
+#define BLOCK_SIZE 16
+
+int main(void){
+
+
+int Left_matrix_x,Left_matrix_y,Right_matrix_x,Right_matrix_y,Left_vector_size, Right_vector_size;
+float *Left_Vector_h,*Right_Vector_h,
+
+}
